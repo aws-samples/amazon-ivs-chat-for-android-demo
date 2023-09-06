5ce1f0f2ea553e0d9aed7a802585f31876e74a3a
Eddy Gorbunov <edijs.gorbunovs@testdevlab.com>
2023-09-06 08:53:55 +0300

    Fixed build issue

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.1")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.46.1")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
+}
